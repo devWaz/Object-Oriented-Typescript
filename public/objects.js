58a134329a10c9aaaa173673e5cd70c93829bb31
fawaz <fawazodessy@gmail.com>
2023-02-14 10:39:08 +0100

    Constructu

--- public/objects.js
+++ public/objects.js
@@ -1,12 +1,27 @@
 "use strict";
-var userOne = {
-    email: 'waz@angelassist.com',
-    name: 'Waz',
-    login() {
-        console.log(`${this.email} has logged in`);
-    },
-    logout() {
-        console.log(`${this.email} has logged out`);
+// var userOne = {
+//     email: 'waz@angelassist.com',
+//     name: 'Waz',
+//     login():void {
+//         console.log(`${this.email} has logged in`);
+//     },
+//     logout():void {
+//         console.log(`${this.email} has logged out`);
+//     }
+// }
+// console.log(userOne.logout());
+// Class Constructors
+class User {
+    constructor(email, name) {
+        this.email = email,
+            this.name = name;
     }
-};
-console.log(userOne.logout());
+}
+var userOne = new User('Waz@angelassist.org', 'Waz');
+console.log(userOne);
+var userTwo = new User('Meen@angelassist.org', 'Meen');
+console.log(userTwo);
+// the 'new' keyword
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
--- src/objects.ts
+++ src/objects.ts
@@ -1,12 +1,32 @@
-var userOne = {
-    email: 'waz@angelassist.com',
-    name: 'Waz',
-    login():void {
-        console.log(`${this.email} has logged in`);
-    },
-    logout():void {
-        console.log(`${this.email} has logged out`);
+// var userOne = {
+//     email: 'waz@angelassist.com',
+//     name: 'Waz',
+//     login():void {
+//         console.log(`${this.email} has logged in`);
+//     },
+//     logout():void {
+//         console.log(`${this.email} has logged out`);
+//     }
+// }
+
+// console.log(userOne.logout());
+
+// Class Constructors
+class User {
+    email: string;
+    name: string;
+    constructor(email:string , name:string){
+        this.email = email,
+        this.name = name
     }
 }
 
-console.log(userOne.logout());
+var userOne = new User('Waz@angelassist.org' , 'Waz')
+console.log(userOne);
+var userTwo = new User('Meen@angelassist.org' , 'Meen')
+console.log(userTwo);
+
+// the 'new' keyword
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
